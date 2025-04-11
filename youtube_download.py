3b88105c5a808671299dac38eff1345037b9e3fa
fenrir429 <75594996+fenrir429@users.noreply.github.com>
2025-04-11 18:57:31 -0500

    initial script

---
+++ youtube_download.py
@@ -0,0 +1,20 @@
+import yt_dlp
+
+def download_mp3(url, output_path='.'):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'outtmpl': f'{output_path}/%(title)s.%(ext)s',
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',
+            'preferredcodec': 'mp3',
+            'preferredquality': '192',
+        }],
+        'quiet': False,
+    }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+if __name__ == "__main__":
+    video_url = input("Enter YouTube URL: ")
+    download_mp3(video_url)
